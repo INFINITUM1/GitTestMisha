9b906b97bf4e7e9b53463a5eb7bf9a1db295947e
Yaroslav <oxxxymirondvp@gmail.com>
2024-06-23 20:26:42 +0300

    second version

--- firstProgram/main.java
+++ firstProgram/main.java
@@ -3,5 +3,11 @@ package firstProgram;
 public class main  {
     public static void main (String[] args) {
         System.out.println ("Hello, World!") ; 
+        System.out.println ("Hello, World!_second") ;
+    }
+
+    public static void notMain (String[] args) {
+        System.out.println ("Hello, World!") ; 
+        System.out.println ("Hello, World!_third") ;
     }
 }
