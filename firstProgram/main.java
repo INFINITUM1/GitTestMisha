4133ab311fbd990f981e60b14d0fdfb070e2616f
Yaroslav <oxxxymirondvp@gmail.com>
2024-06-24 22:55:38 +0300

    охеренній апдейт

--- firstProgram/main.java
+++ firstProgram/main.java
@@ -4,10 +4,7 @@ public class main  {
     public static void main (String[] args) {
         System.out.println ("Hello, World!") ; 
         System.out.println ("Hello, World!_second") ;
-<<<<<<< HEAD
-       
-=======
         System.out.println ("Добавил 3ю строчку") ;
->>>>>>> f687b3211a80ba8b8ccb9a460b6d412d3cf1fdb9
+        System.out.println ("Почти Пятая строчка");
     }
 }
